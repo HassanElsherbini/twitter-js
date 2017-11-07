29caf978bc0ab0b957011e518911ecdc4aaca287
Jeffrey Cheung <jeffreyy.cheungg@gmail.com>
2017-11-07 12:01:02 -0500

    commit nunjucks stuff

--- app.js
+++ app.js
@@ -2,5 +2,21 @@ const app = require('express')();
 const routes = require('./routes/index.js');
 const nunjucks = require('nunjucks');
 
+app.set('view engine', 'html'); // have res.render work with html files
+app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
+nunjucks.configure('views'); // point nunjucks to the proper directory for templates
+
+var locals = {
+    title: 'An Example',
+    people: [
+        { name: 'Gandalf'},
+        { name: 'Frodo' },
+        { name: 'Hermione'}
+    ]
+};
+nunjucks.configure('views', {noCache: true});
+app.get('/', function(req, res) {
+	res.render('index.html',locals);
+})
 app.use('/', routes);
 app.listen(3000, () => console.log(`listening 3000`));
--- routes/index.js
+++ routes/index.js
@@ -1,29 +1,19 @@
 const express = require('express');
 const router = express.Router();
+// const nunjucks = require('nunjucks');
 
 router.use(function (req, res, next) {
     // do your logging here
     // call `next`, or else your app will be a black hole — receiving requests but never properly responding
     // if (err) throw err;
 
-    console.log('Request URL:', req.originalUrl)
-    next()}, 
-    function (req, res, next) {
-      console.log('Request Type:', req.method)
+    console.log(req.method, req.url);
     next()
 })
 
-router.get('/', (req, res) => {
-  res.send(`
-    <html>
-     <head>
-       <title>fakeTwitter</title>
-     </head>
-     <body>
-       <h1>Welcome to fakeTwitter!</h1>
-     </body>
-    </html>
-  `)
-})
+// router.get('/', (req, res) => {
+//   res.render('index.html', locals);
+// })
+
 
 module.exports = router;
